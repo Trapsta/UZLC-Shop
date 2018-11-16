a6e235b400f6ca04b9dcac371b3aa7412c37ef70
unknown <jasper.kinoti@gmail.com>
2018-11-16 11:43:11 +0300

    deploy fixes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use('/api', routes.main);
 app.use('/api/accounts', routes.account);
